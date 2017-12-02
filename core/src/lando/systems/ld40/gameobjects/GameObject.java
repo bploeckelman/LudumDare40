a8fae714e430130126601a231fdb504f598e86cf
Doug Graham <dsgraham@gmail.com>
2017-12-02 14:49:23 -0600

    Added World Stub

--- core/src/lando/systems/ld40/gameobjects/GameObject.java
+++ core/src/lando/systems/ld40/gameobjects/GameObject.java
@@ -2,6 +2,7 @@ package lando.systems.ld40.gameobjects;
 
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Rectangle;
+import lando.systems.ld40.utils.Assets;
 
 
 public class GameObject {
@@ -9,7 +10,7 @@ public class GameObject {
     public Rectangle bounds;
 
     public GameObject(){
-        bounds = new Rectangle(0,0,64, 64);
+        bounds = new Rectangle(120,120,64, 64);
     }
     
     public void update(float dt){
@@ -17,6 +18,6 @@ public class GameObject {
     }
 
     public void render (SpriteBatch batch){
-
+        batch.draw(Assets.testTexture, bounds.x, bounds.y, bounds.width, bounds.height);
     }
 }
