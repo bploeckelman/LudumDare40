7f78b5bdf6c92e1b98d9bff20327f5b088a2a174
Brian Ploeckelman <brian.ploeckelman@gmail.com>
2017-12-02 15:32:25 -0600

    Start fleshing out world, git ignore sprite atlas

---
+++ core/src/lando/systems/ld40/gameobjects/Tile.java
@@ -0,0 +1,24 @@
+package lando.systems.ld40.gameobjects;
+
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import lando.systems.ld40.utils.Assets;
+import lando.systems.ld40.world.World;
+
+public class Tile extends GameObject {
+
+    private Color tempColor = new Color(50 / 255f, 115 / 255f, 69 / 255f, 1f);
+
+    public Tile() {
+        texture = Assets.whitePixel;
+        setSize(World.tile_pixels_wide, World.tile_pixels_high);
+    }
+
+    @Override
+    public void render(SpriteBatch batch) {
+        batch.setColor(tempColor);
+        super.render(batch);
+        batch.setColor(Color.WHITE);
+    }
+
+}
