9296e7a0a686d147aa2d4fc03f13aab2efdbad4f
brossman <brossman@gristmillstudios.com>
2017-12-02 15:00:10 -0600

    updated game object

--- core/src/lando/systems/ld40/gameobjects/Building.java
+++ core/src/lando/systems/ld40/gameobjects/Building.java
@@ -1,5 +1,21 @@
 package lando.systems.ld40.gameobjects;
 
-public class Building extends GameObject{
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
+import lando.systems.ld40.utils.Assets;
+
+public class Building extends GameObject {
+
+    public Color _color;
+    public Building(Color color) {
+        _color = color;
+    }
+    
+    @Override
+    public void render(SpriteBatch batch) {
+        batch.setColor(_color);
+        batch.draw(Assets.whitePixel, bounds.x, bounds.y, bounds.width, bounds.height);
+        batch.setColor(Color.WHITE);
+    }
 }
--- core/src/lando/systems/ld40/gameobjects/GameObject.java
+++ core/src/lando/systems/ld40/gameobjects/GameObject.java
@@ -1,22 +1,69 @@
 package lando.systems.ld40.gameobjects;
 
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector2;
 
 
 public class GameObject {
 
     public Rectangle bounds;
+    public Vector2 position;
+        
+	public float animStateTime = 0;
+    
+    protected float bounds_offset_x;
+    protected float bounds_offset_y;
 
-    public GameObject(){
+    public TextureRegion keyframe;
+
+    public GameObject() {
         bounds = new Rectangle(0,0,64, 64);
     }
     
-    public void update(float dt){
-
+    protected void setKeyFrame(TextureRegion textureRegion) {
+        keyframe = textureRegion;
+        setSize(keyframe.getRegionWidth(), keyframe.getRegionHeight());
     }
 
-    public void render (SpriteBatch batch){
+    public void setX(float x) {
+        position.x = x;
+        bounds.x = position.x - bounds_offset_x;
+    }
 
+    public void setY(float y) {
+        position.y = y;
+        bounds.y = position.y - bounds_offset_y;
+    }
+
+    public void setLocation(float x, float y) {
+        setX(x);
+        setY(y);
+    }
+
+    public void setSize(float width, float height) {
+        bounds.width = width;
+        bounds.height = height;
+
+        bounds_offset_x = width / 2;
+        bounds_offset_y = height /2;
+    }
+    
+    public void setBoundsLocation(float x, float y) {
+        setLocation(x + bounds_offset_x, y + bounds_offset_y);
+    }
+
+    public void update(float dt) {
+        animStateTime += dt;
+    }
+
+    public void render(SpriteBatch batch) {
+        bounds.x = position.x - bounds_offset_x;
+        bounds.y = position.y - bounds_offset_y;
+
+        if (keyframe != null) {
+            batch.draw(keyframe, bounds.x, bounds.y, bounds.width, bounds.height);
+        }
     }
 }
