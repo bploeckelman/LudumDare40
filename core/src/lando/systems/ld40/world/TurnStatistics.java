3a6e89f042049157d8a4b30bf269ef984987a8be
Doug Graham <dsgraham@gmail.com>
2017-12-03 16:25:47 -0600

    Added some stats to track

--- core/src/lando/systems/ld40/world/TurnStatistics.java
+++ core/src/lando/systems/ld40/world/TurnStatistics.java
@@ -8,8 +8,18 @@ public class TurnStatistics {
      */
     public int money;
     public int buildings;
+    public int addons;
+    public int garbageGenerated;
+    public int garbageHauled;
+    public int garbageInLandFills;
+
 
     public TurnStatistics(int money) {
         this.money = money;
+        this.buildings = 0;
+        this.addons = 0;
+        this.garbageGenerated = 0;
+        this.garbageHauled = 0;
+        this.garbageInLandFills = 0;
     }
 }
