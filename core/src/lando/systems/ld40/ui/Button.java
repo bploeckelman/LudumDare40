8fc7dd8477b00a7c1b43233dfbe910b6bb0da789
tpolcyn <tpolcyn@users.noreply.github.com>
2017-12-02 23:16:38 -0600

    Continued refinement of Resolution Phase screen.

--- core/src/lando/systems/ld40/ui/Button.java
+++ core/src/lando/systems/ld40/ui/Button.java
@@ -8,7 +8,6 @@ import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.math.Vector3;
-import com.badlogic.gdx.utils.Align;
 import lando.systems.ld40.utils.Assets;
 import lando.systems.ld40.utils.Config;
 
