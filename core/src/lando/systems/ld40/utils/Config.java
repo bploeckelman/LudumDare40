23f54575fa37174515cf9e9f3f6900d51b360d2e
Doug Graham <dsgraham@gmail.com>
2017-12-04 01:16:02 -0600

    Look and feel

--- core/src/lando/systems/ld40/utils/Config.java
+++ core/src/lando/systems/ld40/utils/Config.java
@@ -13,8 +13,10 @@ public class Config {
     public static Color bgColor = new Color(82f/255f,60f /255f,20f /255f,1);
 
     public static Color COLOR_BACKGROUND = new Color(71/255f, 71/255f, 87/255f, 1f);
+    public static Color COLOR_SHADED = new Color(120/255f, 120/255f, 118/255f, 1f);
     public static Color COLOR_TEXT = new Color(235/255f, 255/255f, 218/255f, 1f);
     public static Color COLOR_BLACK = new Color(23/255f, 13/255f, 32/255f, 1f);
+    public static Color COLOR_GOLD = new Color(235/255f, 208/255f, 0f, 1f);
 
     public static int gameTurns = 25;
 }
